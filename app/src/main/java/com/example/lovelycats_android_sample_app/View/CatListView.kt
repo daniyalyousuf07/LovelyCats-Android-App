bcc9dd2b3fde3a52e74a432f4eb1c0330dd6c4e5
Daniyal <daniyalyousuf44@gmail.com>
2023-10-13 00:44:24 -0400

    [loader type updated]

--- app/src/main/java/com/example/lovelycats_android_sample_app/View/CatListView.kt
+++ app/src/main/java/com/example/lovelycats_android_sample_app/View/CatListView.kt
@@ -20,6 +20,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -50,10 +51,11 @@ fun CatListViewRendering(navigationCallBack: (BreedModel) -> Unit) {
     val breed = viewModel.catListState.value
     var loading = viewModel.isLoading.value!!
     if (loading) {
-        LinearProgressIndicator(
+        CircularProgressIndicator(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(12.dp)
+                .padding(12.dp),
+            color = MaterialTheme.colorScheme.primary,
+            strokeWidth = 2.dp,
         )
     } else {
         LazyColumn(contentPadding = PaddingValues(16.dp)) {
